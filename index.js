384d4814472c6dbf9e82d8d3f0d2db0f0f921270
riyadh hossain <riyadh_h@ymail.com>
2018-07-28 01:36:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ function johnLennonFacts(array,facts){
 }
 
 
-/*function iLoveTheBeatles(number){
+function iLoveTheBeatles(number){
   var array = []
   do{
     array.push("I love the Beatles!")
   }while(number<15)
   return array;
-} */
+}
